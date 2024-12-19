b2cf61194baf407a3182d7c38f89c81d4a6bcdad
Kukushouhou <1@kukushouhou.com>
2024-12-19 21:07:56 +0800

    - build

--- dist/cjs/index.d.ts
+++ dist/cjs/index.d.ts
@@ -1,4 +1,3 @@
 import proxy from './proxy';
 import hook from "./hook";
 export { proxy, hook };
-//# sourceMappingURL=index.d.ts.map
--- dist/cjs/proxy.xhr.d.ts
+++ dist/cjs/proxy.xhr.d.ts
@@ -5,4 +5,3 @@ declare global {
     }
 }
 export default function proxyXHR(options: ProxyOptions, win: Window): void;
-//# sourceMappingURL=proxy.xhr.d.ts.map
